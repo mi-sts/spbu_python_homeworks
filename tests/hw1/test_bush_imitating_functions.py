d4811b4c8fe1806b7a265c9808181673d676ef34
mi-sts <66365072+mi-sts@users.noreply.github.com>
2022-02-28 02:38:32 +0300

    Code style

--- tests/hw1/test_bush_imitating_functions.py
+++ tests/hw1/test_bush_imitating_functions.py
@@ -11,77 +11,76 @@ def temp_file(tmp_path):
     temp_file = temp_dir / filename
     return temp_file
 
-@pytest.mark.parametrize("data, output", [
-    (
+
+@pytest.mark.parametrize(
+    "data, output",
+    [
+        (
             "qwerty uiop 123\nasd fgh hjk\n456 7 zxc 89",
             "3 10 40",
-    ),
-    (
-            "",
-            "0 0 0"
-    ),
-    (
-            "zxc vbn",
-            "1 2 7"
-    )
-])
+        ),
+        ("", "0 0 0"),
+        ("zxc vbn", "1 2 7"),
+    ],
+)
 def test_wc(data, output, temp_file, capfd):
     temp_file.write_text(data)
     wc(temp_file.absolute())
     out, err = capfd.readouterr()
     assert out == "{0} {1}\n".format(output, temp_file.absolute())
 
-@pytest.mark.parametrize("data, output", [
-    (
+
+@pytest.mark.parametrize(
+    "data, output",
+    [
+        (
             "qwerty uiop 123\nasd fgh hjk\n456 7 zxc 89",
             "   1 qwerty uiop 123\n   2 asd fgh hjk\n   3 456 7 zxc 89\n",
-    ),
-    (
-            "zxc vbn",
-            "   1 zxc vbn\n"
-    )
-])
+        ),
+        ("zxc vbn", "   1 zxc vbn\n"),
+    ],
+)
 def test_nl(data, output, temp_file, capfd):
     temp_file.write_text(data)
     nl(temp_file.absolute())
     out, err = capfd.readouterr()
     assert out == output
 
-@pytest.mark.parametrize("data, lines_number, output", [
-    (
-            "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67",
-            5,
-            "qw\ner\nty\nui\nop\n"
-    ),
-    (
+
+@pytest.mark.parametrize(
+    "data, lines_number, output",
+    [
+        ("qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67", 5, "qw\ner\nty\nui\nop\n"),
+        (
             "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67",
             10,
-            "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\n"
-    )
-])
+            "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\n",
+        ),
+    ],
+)
 def test_head(data, lines_number, output, temp_file, capfd):
     temp_file.write_text(data)
     head(temp_file.absolute(), lines_number)
     out, err = capfd.readouterr()
     assert out == output
 
-@pytest.mark.parametrize("data, lines_number, output", [
-    (
-            "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67",
-            5,
-            "bn\nm1\n23\n45\n67\n"
-    ),
-    (
+
+@pytest.mark.parametrize(
+    "data, lines_number, output",
+    [
+        ("qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67", 5, "bn\nm1\n23\n45\n67\n"),
+        (
             "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67",
             10,
-            "gh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67\n"
-    ),
-    (
+            "gh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67\n",
+        ),
+        (
             "qw\ner\nty\nui\nop\nas\ndf\ngh\njk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67\n",
             10,
-            "jk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67\n\n"
-    )
-])
+            "jk\nlz\nxc\ncv\nbn\nm1\n23\n45\n67\n\n",
+        ),
+    ],
+)
 def test_tail(data, lines_number, output, temp_file, capfd):
     temp_file.write_text(data)
     tail(temp_file.absolute(), lines_number)
