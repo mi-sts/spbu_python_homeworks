421eea42cfcd8be6ca899f2900c9f0ff52fc4079
mi-sts <66365072+mi-sts@users.noreply.github.com>
2022-03-08 12:37:48 +0300

    Added the uncurry function and made some changes in curry function

--- src/hw2/curry.py
+++ src/hw2/curry.py
@@ -2,18 +2,36 @@ from typing import Callable
 
 
 def curry_explicit(function: Callable, arity: int):
-    args = []
+    if arity < 0:
+        raise ValueError("Arity can't be negative!")
 
-    def curry_func_tail(argument):
-        if len(args) == arity:
-            raise AttributeError("Too many arguments in function!")
+    if arity == 0 or arity == 1:
+        return function
 
-        args.append(argument)
+    def arguments_accumulator(*args):
         if len(args) == arity:
             return function(*args)
 
-        return curry_func_tail
+        return lambda next_argument: arguments_accumulator(*args, next_argument)
 
-    return curry_func_tail
+    return arguments_accumulator()
 
-def un
+
+def uncurry_explicit(function: Callable, arity: int):
+    if arity < 0:
+        return ValueError("Arity can't be negative!")
+
+    if arity == 0 or arity == 1:
+        return function
+
+    def uncurried_function(*args):
+        if len(args) != arity:
+            raise AttributeError("Incorrect number of arguments in the function!")
+
+        nonlocal function
+        for i in args[:-1]:
+            function = function(i)
+
+        return function(args[-1])
+
+    return uncurried_function
