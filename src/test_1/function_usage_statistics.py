7435f516c60e29ac02a646863c2784ecca7dc384
mi-sts <66365072+mi-sts@users.noreply.github.com>
2022-03-25 15:07:41 +0300

    Add solution for test task_1

---
+++ src/test_1/function_usage_statistics.py
@@ -0,0 +1,28 @@
+from typing import Callable
+from typing import Generator
+
+
+def spy(function: Callable):
+    from datetime import datetime
+
+    def usage_statistics_generator() -> tuple:
+        for i in usage_statistics:
+            yield i
+
+    usage_statistics = []
+
+    def wrapper(*args, **kwargs) -> object:
+        function_return = function(*args, **kwargs)
+        usage_statistics.append((datetime.now(), (*args, *kwargs)))
+        return function_return
+
+    wrapper.statistics_generator = usage_statistics_generator
+
+    return wrapper
+
+
+def print_usage_statistic(function: Callable) -> object:
+    try:
+        return function.statistics_generator()
+    except AttributeError:
+        print("This function is not decorated with spy!")
