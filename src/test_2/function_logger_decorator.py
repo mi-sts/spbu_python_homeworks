886c250edb3a237db5bcd320717fa8504c295d35
mi-sts <66365072+mi-sts@users.noreply.github.com>
2022-05-27 22:44:51 +0300

    Add tests

--- src/test_2/function_logger_decorator.py
+++ src/test_2/function_logger_decorator.py
@@ -7,17 +7,17 @@ def logger(file_path: str):
     def _get_logging_text(func: Callable, args, kwargs, func_result, call_datetime: str) -> str:
         func_name = func.__name__
         kwargs_str = str(kwargs)[1:-1]
-        args_str = ", ".join(str(i) for i in args)
+        args_str = str(args)[1:-1]
         return f"{call_datetime} {func_name} ({args_str}) ({kwargs_str}) {func_result}\n"
-
-    file = open(file_path, "a+")
 
     def decorator(func: Callable):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            file = open(file_path, "a+")
             func_result = func(*args, **kwargs)
             call_datetime = strftime("%d/%m/%Y %H:%M:%S", gmtime())
             file.write(_get_logging_text(func, args, kwargs, func_result, call_datetime))
+            file.close()
 
             return func_result
 
